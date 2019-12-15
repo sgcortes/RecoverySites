eed131c8c35761776933080153d543cb1f38f21f
Silverio Garcia-Cortes <sgcortes@uniovi.es>
2019-12-15 20:40:22 +0100

    commit1

---
+++ data/RecoverySitesEPSG4326_2.js
@@ -0,0 +1,12 @@
+var json_RecoverySitesEPSG4326_2 = {
+"type": "FeatureCollection",
+"name": "RecoverySitesEPSG4326_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "2", "name": "Janina", "country": "Poland", "gisweb": "https:\/\/sgcortes.github.io\/Janina\/", "Lat(WGS84)": 50.081137, "Long(WGS84": 19.305746, "Image": "<img src = \".\/images\/janina.jpg\"\/>" }, "geometry": { "type": "Point", "coordinates": [ 19.305746114367434, 50.081136805446256 ] } },
+{ "type": "Feature", "properties": { "id": "3", "name": "Terezie", "country": "Czech Rep.", "gisweb": "https:\/\/sgcortes.github.io\/Terezie\/", "Lat(WGS84)": 49.840756, "Long(WGS84": 18.31694, "Image": "<img src = \".\/images\/Ema.jpg\" \/>" }, "geometry": { "type": "Point", "coordinates": [ 18.316939880656488, 49.840756041083658 ] } },
+{ "type": "Feature", "properties": { "id": "4", "name": "Chabarovice", "country": "Czech Rep.", "gisweb": "https:\/\/sgcortes.github.io\/Chabarovice", "Lat(WGS84)": 50.653763, "Long(WGS84": 13.95198, "Image": "<img src = \".\/images\/Chabarovice.jpg\" \/>" }, "geometry": { "type": "Point", "coordinates": [ 13.951980128250282, 50.653763035960374 ] } },
+{ "type": "Feature", "properties": { "id": "5", "name": "Most-Lezaky", "country": "Czech Rep.", "gisweb": "https:\/\/sgcortes.github.io\/Most-Lezaky\/", "Lat(WGS84)": 50.541477, "Long(WGS84": 13.647139, "Image": "<img src = \".\/images\/Most.jpg\" \/>" }, "geometry": { "type": "Point", "coordinates": [ 13.647138967933479, 50.541476877219978 ] } },
+{ "type": "Feature", "properties": { "id": "1", "name": "Figaredo", "country": "Spain", "gisweb": "https:\/\/sgcortes.github.io\/RECOVERY19\/", "Lat(WGS84)": 43.20475, "Long(WGS84": -5.767042, "Image": "<img src = \".\/images\/Figaredo2.jpg\"\/>" }, "geometry": { "type": "Point", "coordinates": [ -5.767042344748845, 43.204749912590103 ] } }
+]
+}
